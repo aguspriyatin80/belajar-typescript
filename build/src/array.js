5e4b516fc28673fabcb811a3ca0fbce916bce2bf
Agus Priyatin <aguspriyatin80>
2021-08-21 22:44:28 +0700

    array tuple

--- build/src/array.js
+++ build/src/array.js
@@ -5,3 +5,9 @@ var array2;
 array2 = ['string 1', 'string 2'];
 var array3;
 array3 = [1, 'string', true, [], {}];
+// tuples : tipe data seperti array tetapi nilai atau itemnya terbatas
+var biodata;
+biodata = ["Surabaya", 123];
+biodata = ["Cirebon", true];
+biodata = ["Jakarta", 100, "string"];
+biodata = ["Jakarta", 100, true];
--- src/array.ts
+++ src/array.ts
@@ -7,3 +7,9 @@ array2 = ['string 1', 'string 2']
 let array3: any[]
 array3 = [1, 'string', true, [], {}]
 
+// tuples : tipe data seperti array tetapi nilai atau itemnya terbatas
+let biodata: [string, number]
+biodata = ["Surabaya", 123]
+biodata = ["Cirebon", true]
+biodata = ["Jakarta", 100, "string"]
+biodata = ["Jakarta", 100, true]
