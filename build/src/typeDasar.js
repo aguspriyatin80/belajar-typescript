ec4f92dca1222aa29da12e7a7efc87e08abe2450
Agus Priyatin <aguspriyatin80>
2021-08-21 22:32:12 +0700

    tipe data number & boolean

--- build/src/typeDasar.js
+++ build/src/typeDasar.js
@@ -1,3 +1,12 @@
 "use strict";
+// string
 var nama = "Agus";
 nama = "string";
+// number
+var umur = 30;
+umur = 25;
+umur = 13;
+// boolean
+var isMarried;
+isMarried = true;
+isMarried = false;
--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -1,2 +1,13 @@
+// string
 let nama: string = "Agus";
 nama = "string";
+
+// number
+let umur: number = 30
+umur = 25
+umur = 13
+
+// boolean
+let isMarried: boolean
+isMarried = true
+isMarried = false
