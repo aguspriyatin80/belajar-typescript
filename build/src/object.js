76d17a831e9b9bcc62d2df2eba00b1baf1fa98da
Agus Priyatin <aguspriyatin80>
2021-08-21 23:45:07 +0700

    part11 - tipe data object

---
+++ build/src/object.js
@@ -0,0 +1,10 @@
+"use strict";
+// tipe data object
+var user = {
+    name: "Agus",
+    age: 30
+};
+user = {
+    name: "Eka",
+    age: 12
+};
---
+++ src/object.ts
@@ -0,0 +1,15 @@
+// tipe data object
+
+type User = {
+    name: string,
+    age: number
+}
+
+let user = {
+    name: "Agus",
+    age: 30
+}
+user = {
+    name: "Eka",
+    age: 12
+}
