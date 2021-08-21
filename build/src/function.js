cce8be40109b311b1138da9aab64d97c3efd9625
Agus Priyatin <aguspriyatin80>
2021-08-21 23:09:05 +0700

    function

---
+++ build/src/function.js
@@ -0,0 +1,12 @@
+"use strict";
+// tipe data balikan function
+function getName() {
+    return 'Agus Priyatin';
+}
+function getAge() {
+    return 33;
+}
+function printName() {
+    console.log("Print name");
+}
+printName();
---
+++ src/function.ts
@@ -0,0 +1,13 @@
+// tipe data balikan function
+
+function getName(): string {
+    return 'Agus Priyatin'
+}
+function getAge(): number {
+    return 33
+}
+
+function printName(): void {
+    console.log("Print name")
+}
+printName();
